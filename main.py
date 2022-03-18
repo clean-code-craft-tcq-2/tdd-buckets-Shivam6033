770d29daaf8ad6db38658d2c6f449bbf527f9f5b
Shivam (MS/ESD-ET2-XC) <tgh1kor@bosch.com>
2022-03-18 17:09:49 +0530

    Expanded get_range function for wider use case!!
    Signed-off-by: Shivam (MS/ESD-ET2-XC) <tgh1kor@bosch.com>

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
+from collections import Counter
+
+
+def get_consecutive_sublist(lis):
+    pointer = 0
+    main_list = []
+    sub_list = []
+    while pointer < len(lis):
+        if pointer == 0:
+            sub_list.append(lis[pointer])
+            pointer = pointer + 1
+            continue
+        else:
+            if lis[pointer] == lis[pointer - 1]:
+                pointer = pointer + 1
+                continue
+            elif lis[pointer] == lis[pointer - 1] + 1:
+                sub_list.append(lis[pointer])
+            elif lis[pointer] != lis[pointer - 1] + 1:
+                main_list.append(sub_list)
+                sub_list = []
+                sub_list.append(lis[pointer])
+        if pointer == len(lis) - 1:
+            main_list.append(sub_list)
+        pointer += 1
+    return main_list
+
+
+def get_items_count(iterable: list):
+    return dict(Counter(iterable))
+
+
+def get_ranges_count(sublist, items_count_mapper):
+    string = ""
+    for sub_list in sublist:
+        summation = 0
+        if len(sub_list) > 1:
+            string = string + f"{sub_list[0]}-{sub_list[-1]}"
+            for items in range(sub_list[0], sub_list[-1] + 1):
+                summation = summation + items_count_mapper[items]
+            string = string + f" : {summation}\n"
+    return string
+
 
 def get_range_reading(values: list) -> str:
     values.sort()
-    return f'{values[0]}-{values[-1]}, {len(values)}'
+    sublist = get_consecutive_sublist(values)
+    items_count_mapper = get_items_count(values)
+    string = get_ranges_count(sublist, items_count_mapper)
+    return string
--- test.py
+++ test.py
@@ -1,12 +1,31 @@
 import main
 
 
-def test_get_range_reading():
-    input_list = [4, 5]
-    expected_output = "4-5, 2"
-    returned_value = main.get_range_reading(input_list)
-    assert expected_output == returned_value
+def test_get_range_reading_double_values():
+    input_list = [[4, 5], [7, 8], [9, 10], [1, 5]]
+    expected_output = ["4-5 : 2\n", "7-8 : 2\n", "9-10 : 2\n", ""]
+    for indx, lists in enumerate(input_list):
+        returned_value = main.get_range_reading(lists)
+        assert expected_output[indx] == returned_value
+
+
+def test_get_range_reading_single_values():
+    input_list = [[4], [0], [3]]
+    expected_output = ""
+    for lists in input_list:
+        returned_value = main.get_range_reading(lists)
+        assert expected_output == returned_value
+
+
+def test_get_range_reading_multiple_values():
+    input_list = [[4, 5, 5, 7, 7, 7, 8], [1, 3, 3, 4, 5, 8, 9, 10], [3, 3, 5, 4, 10, 11, 12]]
+    expected_output = ["4-5 : 3\n7-8 : 4\n", "3-5 : 4\n8-10 : 3\n", "3-5 : 4\n10-12 : 3\n"]
+    for indx, lists in enumerate(input_list):
+        returned_value = main.get_range_reading(lists)
+        assert expected_output[indx] == returned_value
 
 
 if __name__ == "__main__":
-    test_get_range_reading()
+    test_get_range_reading_single_values()
+    test_get_range_reading_double_values()
+    test_get_range_reading_multiple_values()
